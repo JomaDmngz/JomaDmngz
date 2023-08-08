534ecdbf2e90c597d67db928ff78f1f9e48d0355
JomaDmngz <60558869+JomaDmngz@users.noreply.github.com>
2023-08-08 23:47:49 +0000

    new file:   .vscode/launch.json new file:   .vscode/tasks.json new file:   inventory_admin/main.py new file:   main.c

---
+++ inventory_admin/main.py
@@ -0,0 +1,8 @@
+from math import cos
+
+i = (1,2,3,4,5)
+
+for n in i:
+    area = n + cos(n)
+    print("Hi! ", area)
+
---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+static void main(){
+    printf("Hola Mundo!");
+}
